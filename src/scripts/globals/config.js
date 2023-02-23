a0f72a027619978b0eeeee6a26378770d5035773
rohmads <rohmadsubekti48@gmail.com>
2023-02-23 16:02:04 +0700

    fix config api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '388da65ae21aefc6e1353baee1546aeb',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
